7a2b763e52bc3f93d17bb4d7a580a55978cded5f
DOROTHY CHEPKOECH <chepkoechdorothy209@gmail.com>
2025-02-25 11:33:31 +0300

    Finish assignment

---
+++ index.js
@@ -0,0 +1,56 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+ function logWhisper(string) {
+    console.log(string.toLowerCase()); // inafaa ikue console.log(string.toLowerCase())
+ }
+function 
+sayHiToHeadphonedRoommate (string) {
+    if (string === string.toLowerCase()) {
+        return "I can't hear you!";
+    } else if (string === string.toUpperCase())
+    {
+        return "YES INDEED!";
+    }else if (string === "Let's have dinner together!")
+         {
+     return "I would love to!";   
+    }    
+}    
+  
+
+
+/*function shout(string) {
+    return string.toUpperCase();
+  }
+  
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  
+  function logShout(string) {
+    console.log(string.toUpperCase());
+  }
+  
+  function logWhisper(string) {
+    console.log(string.toLowerCase());
+  }
+  
+  function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+      return "I can't hear you!";
+    } else if (string === string.toUpperCase()) {
+      return "YES INDEED!";
+    } else if (string === "Let's have dinner together!") {
+      return "I would love to!";
+    }
+  }
+
+  */
+ // the commented out ndo my code and it passes the 7 test. so go through it
+ //alafu uone the problem iko wapi then uncomment it
